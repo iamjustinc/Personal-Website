98cbda374d2dda4111a93fc3652b820dbf71c704
iamjustinc <justin886847@gmail.com>
2026-04-16 21:23:18 -0700

    feat: implement guided chat and identity fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import { Hero } from '@/components/hero/Hero'
-import { CapabilityStrip } from '@/components/sections/CapabilityStrip'
 import { ProjectsSection } from '@/components/sections/ProjectsSection'
 import { CareerSection } from '@/components/sections/CareerSection'
 import { AboutSection } from '@/components/sections/AboutSection'
 import { ContactSection } from '@/components/sections/ContactSection'
-import { siteConfig } from '@/data/site'
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      {siteConfig.showCapabilityStrip && <CapabilityStrip />}
       <ProjectsSection />
       <AboutSection />
       <CareerSection/>
